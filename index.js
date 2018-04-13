7b52816203dfc0ee8230e44c8c11bafa081488cb
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-04-13 16:53:55 +0200

    stat pollin

--- index.js
+++ index.js
@@ -1,69 +1,60 @@
 var { Readable } = require('stream')
-var { createReadStream, lstat, open, read, stat } = require('fs')
+var { lstat, open, read, stat, watchFile } = require('fs')
 var debug = require('debug')('append-only-live-stream')
 
 function chiefstat (file, opts, cb) {
   opts.dereference ? stat(file, cb) : lstat(file, cb)
 }
 
-function oncereadable () {
-  var chunk = this._readStream.read()
-  debug('read chunk::', chunk, 'aka', (chunk || '').toString())
-  if (chunk) {
-    this._bytesTransmitted += chunk.length
-    var more = this.push(chunk)
-    if (more) this._read()
-  }
-  else {
-    this._read()
-  }
-}
-
-function onstat (file, err, stats) {
-  debug('onstat err::', err)
-  if (err) return this.emit('error', err)
-  debug('stats.size, bytesTransmitted::', stats.size, this._bytesTransmitted)
-  if (stats.size > this._bytesTransmitted) {
-    open(file, 'r', onopen.bind(this, stats))
-  }
-  else {
-    this._read()
-  }
-}
-
-function onopen (stats, err, fd) {
+function onopen (err, fd) {
   debug('onopen err::', err)
   if (err) return this.emit('error', err)
-  var diff = stats.size - this._bytesTransmitted
+  var diff = this._filesize - this._bytespushed
+  debug('diff::', diff)
   var buf = Buffer.alloc(diff)
-  read(fd, buf, 0, diff, this._bytesTransmitted + 1, onread.bind(this))
+  read(fd, buf, 0, diff, this._bytespushed, onread.bind(this))
 }
 
 function onread (err, bytesRead, buf) {
   debug('onread err::', err)
   if (err) return this.emit('error', err)
-  this._bytesTransmitted += buf.length
+  this._bytespushed += buf.length
+  debug('pushin::', buf)
   var more = this.push(buf)
   if (more) this._read()
+}
+
+function oninitstat (err, stats) {
+  if (err) return this.emit('error', err)
+  this._filesize = stats.size
+  debug('pulled stat file size::', this._filesize)
+}
+
+function onwatch (cur, prev) {
+  debug('updating filesize::', cur.size)
+  this._filesize = cur.size
+  if (cur.size > this._bytespushed) this._read()
 }
 
 function createLiveStream (file, opts) {
   if (!opts) opts = {}
   var liveStream = new Readable({
     read () {
-      debug('::readin::')
-      debug('readStream ended?::', this._readStream._readableState.ended)
-      if (!this._readStream._readableState.ended) {
-        debug('::readin from readStream::')
-        this._readStream.once('readable', oncereadable.bind(this))
-      } else {
-        debug('::chiefstatin::')
-        chiefstat(file, opts, onstat.bind(this, file))
+      debug('filesize,bytespushed::', this._filesize, this._bytespushed)
+      if (this._filesize > this._bytespushed) {
+        open(file, 'r', onopen.bind(this))
       }
     }
   })
-  liveStream._readStream = createReadStream(file)
-  liveStream._bytesTransmitted = 0
+  liveStream._opts = Object.assign({ persistent: false, interval: 419 }, opts)
+  liveStream._bytespushed = 0
+  liveStream._filesize = 0
+  liveStream._destroy = function _destroy (cb) {
+    unwatchFile(file)
+    cb()
+  }
+  chiefstat(file, liveStream._opts, oninitstat.bind(liveStream))
+  watchFile(file, liveStream._opts, onwatch.bind(liveStream))
   return liveStream
 }
 
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var { createWriteStream, unlink } = require('fs')
+var { createWriteStream, unlink, unlinkSync, writeFile } = require('fs')
 var tape = require('tape')
 var createLiveStream = require('./index')
 
@@ -10,74 +10,63 @@ tape.onFinish(
 
 tape('live - trivial', function (t) {
   var appendStream = createWriteStream('./trivial.test', { flags: 'a' })
-  appendStream.write('419')
-  var liveStream = createLiveStream('./trivial.test')
-  var chunks = []
-  liveStream.on('data', function (chunk) {
-    chunks.push(chunk.toString())
+  appendStream.write('419', function () { // allow data to arrive on disk
+    var liveStream = createLiveStream('./trivial.test')
+    var chunks = []
+    liveStream.on('data', function (chunk) {
+      chunks.push(chunk.toString())
+    })
+    for (var i = 8; i > -1; i--) appendStream.write('' + i)
+    setTimeout(function () {
+      t.is(chunks.join(''), '419876543210', 'got live chunks')
+      t.end()
+    }, 1000)
   })
-  for (var i = 8; i > -1; i--) {
-    appendStream.write('' + i)
-    if (!i) {
-      setTimeout(function () {
-        t.is(chunks.join(''), '419876543210', 'got live chunks')
-        t.end()
-      }, 500)
-    }
-  }
 })
 
 tape('live - medium', function (t) {
   var appendStream = createWriteStream('./medium.test', { flags: 'a' })
-  appendStream.write('419')
-  var liveStream = createLiveStream('./medium.test')
-  var crunch = ''
-  var expected = '999'
-  liveStream.on('data', function (chunk) {
-    crunch += chunk.toString()
+  appendStream.write('419', function () { // allow data to arrive on disk
+    var liveStream = createLiveStream('./medium.test')
+    var crunch = ''
+    var expected = '999'
+    liveStream.on('data', function (chunk) {
+      crunch += chunk.toString()
+    })
+    for (var i = 0, end = 1e3 - 1; i < 1e3; i++) appendStream.write('' + i)
+    setTimeout(function () {
+      var actual = crunch.slice(crunch.length - expected.length)
+      t.is(actual, expected, 'right tail')
+      t.end()
+    }, 1000)
   })
-  for (var i = 0, end = 1e3 - 1; i < 1e3; i++) {
-    appendStream.write('' + i)
-    if (i === end) {
-      setTimeout(function () {
-        var actual = crunch.slice(crunch.length - expected.length)
-        t.is(actual, expected, 'right tail')
-        t.end()
-      }, 500)
-    }
-  }
 })
 
-tape.only('live - slow', function (t) {
-  var appendStream = createWriteStream('./slow.test', { flags: 'a' })
-  var liveStream = createLiveStream('./slow.test')
-  var crunch = ''
-  var expected = 'ACAB'
-  var input = expected.split('')
-  liveStream.on('data', function (chunk) {
-    console.log('output', chunk)
-    crunch += chunk.toString()
+tape('live - slow', function (t) {
+  writeFile('./slow.test', '', function (err) {
+    if (err) t.end(err)
+    var appendStream = createWriteStream('./slow.test', { flags: 'a' })
+    var liveStream = createLiveStream('./slow.test')
+    var crunch = ''
+    var expected = 'ACAB'
+    var input = expected.split('')
+    var ended = false
+    liveStream.on('data', function (chunk) {
+      crunch += chunk.toString()
+    })
+    var interval = setInterval(function () {
+      var char = input.shift()
+      if (char) appendStream.write(char)
+      if (!char) {
+        setTimeout(function () {
+          t.is(crunch, expected, 'ACAB')
+          if (!ended) {
+            ended = true
+            t.end()
+          }
+          clearInterval(interval)
+        }, 500)
+      }
+    }, 500)
   })
-  var interval = setInterval(function () {
-    var char = input.shift()
-    console.log('input', char)
-    if (char) appendStream.write(char)
-    if (!input.length || !char) {
-      setTimeout(function () {
-        t.is(crunch, expected, 'ACAB')
-        clearInterval(interval)
-        t.end()
-      }, 500)
-    }
-  }, 250)
-  // for (var i = 0, end = expected.length - 1; i < expected.length; i++) {
-  //   appendStream.write('' + i)
-  //   if (i === end) {
-  //     setTimeout(function () {
-  //       var actual = crunch.slice(crunch.length - expected.length)
-  //       t.is(actual, expected, 'right tail')
-  //       t.end()
-  //     }, 500)
-  //   }
-  // }
 })
