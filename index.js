c3611afbb6f30d8f2ba435e1cc507860605c0240
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-05-10 17:44:59 +0200

    warn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var { Readable } = require('stream')
 var { open, read, stat, watch } = require('fs')
 var debug = require('debug')('append-only-live-stream')
 
-// BUG: something is wrong with stat - sometimes
+// WARNING: unstable - sometimes something is wrong with stat
 
 function onopen (err, fd) {
   debug('onopen err,fd::', err, fd)
